688907800c3ac6f52d797ca332b4e29f694719da
Sergey Lamcev <sergey.lamcev@anoda.mobi>
2019-06-25 09:22:13 +0300

    Start working on video animation

--- photo-to-video-converter/photo-to-video-converter/UserStory/VideoConverter/VideoConverterVM.swift
+++ photo-to-video-converter/photo-to-video-converter/UserStory/VideoConverter/VideoConverterVM.swift
@@ -27,6 +27,10 @@ class VideoConverterVM {
                                                 userInfo: ["description": "AVAssetWriterInputPixelBufferAdapter failed to append pixel buffer"])
     let failedToAppendPixelBufferError = NSError(domain: VideoConverterVM.kErrorDomain, code: 1,
                                                  userInfo: ["description": "AVAssetWriter failed to start writing"])
+    let failedToExportAssetError = NSError(domain: VideoConverterVM.kErrorDomain, code: 2,
+                                           userInfo: ["description": "AVAssetExportSession failed"])
+    let canceledExportAssetError = NSError(domain: VideoConverterVM.kErrorDomain, code: 3,
+                                           userInfo: ["description": "AVAssetExportSession canceled"])
     
     private var videoWriter: AVAssetWriter?
     
@@ -48,8 +52,16 @@ class VideoConverterVM {
         build(photos: photosURLs, { (progress) in
             debugPrint(progress)
         }, success: { [unowned self] (url) in
-            self.delegate?.didFetchVideoURL(url: url)
-            self.delegate?.didReceiveSuccess(message: "Successfully converted video:\n \(url.absoluteString)")
+            //            self.delegate?.didFetchVideoURL(url: url)
+            //            self.delegate?.didReceiveSuccess(message: "Successfully converted video:\n \(url.absoluteString)")
+            self.addAnimationToVideo(videoURL: url, success: { (url) in
+                
+                self.delegate?.didFetchVideoURL(url: url)
+                self.delegate?.didReceiveSuccess(message: "Successfully converted video:\n \(url.absoluteString)")
+                
+            }, failure: { (error) in
+                self.delegate?.didReceivedError(error: error.localizedDescription)
+            })
         }) { [unowned self] (error) in
             self.delegate?.didReceivedError(error: error.localizedDescription)
         }
@@ -160,16 +172,16 @@ extension VideoConverterVM {
                     let currentProgress = Progress(totalUnitCount: Int64(photos.count))
                     
                     for (index, photoURL) in photos.enumerated() {
-
+                        
                         let frameStartTime = index == 0 ? CMTime.zero : CMTimeMake(value: Int64(index) * duration, timescale: fps)
-
+                        
                         let lastFrameTime = CMTimeAdd(frameStartTime, CMTimeMake(value: duration / 2, timescale: fps))
-
+                        
                         error = self.writeFrame(photoURL: photoURL, videoWriterInput: videoWriterInput, pixelBufferAdaptor: pixelBufferAdaptor, frameStartTime: frameStartTime, frameEndTime: lastFrameTime)
-
+                        
                         currentProgress.completedUnitCount = Int64(index+1)
                         progress(currentProgress)
-
+                        
                     }
                     
                     videoWriterInput.markAsFinished()
@@ -274,6 +286,112 @@ extension VideoConverterVM {
         context.draw(cgImage, in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
         
         CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+    }
+    
+    private func addAnimationToVideo(videoURL: URL, success: @escaping ((URL) -> Void), failure: @escaping ((NSError) -> Void)) {
+        
+        let videoAsset: AVURLAsset = AVURLAsset(url: videoURL, options: nil)
+        let mixComposition: AVMutableComposition = AVMutableComposition()
+        
+        guard let compositionVideoTrack = mixComposition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid) else {
+            assertionFailure("Unable to create composition")
+            return
+        }
+        
+        guard let clipVideoTrack = videoAsset.tracks(withMediaType: .video).first else {
+            assertionFailure("Unable to video track")
+            return
+        }
+        let timeRange = CMTimeRange(start: CMTime.zero, end: videoAsset.duration)
+        
+        do {
+            try compositionVideoTrack.insertTimeRange(timeRange, of: clipVideoTrack, at: CMTime.zero)
+        } catch {
+            assertionFailure(error.localizedDescription)
+        }
+        
+        compositionVideoTrack.preferredTransform = clipVideoTrack.preferredTransform
+        
+        guard let videoTrack: AVAssetTrack = videoAsset.tracks(withMediaType: .video).first else {
+            assertionFailure("Unable to video track")
+            return
+        }
+        let videoSize: CGSize = videoTrack.naturalSize
+        
+        //  create the layer with the animation
+        let animationLayer = CALayer()
+        animationLayer.frame = CGRect(x: 0, y: 0, width: videoSize.width, height: videoSize.height)
+        let animation = CATransition()
+        
+        //Set transition properties
+        animation.type = CATransitionType(rawValue: UserDefaultsManager.shared.selectedTransition!)
+        animation.subtype = CATransitionSubtype.init(rawValue: UserDefaultsManager.shared.selectedTransitionDirection!)
+        animation.duration = UserDefaultsManager.shared.selectedTransitionDuration!
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        
+        animationLayer.add(animation, forKey: "animation")
+        
+        //sorts the layer in proper order
+        let parentLayer = CALayer()
+        let videoLayer = CALayer()
+        parentLayer.frame = CGRect(x: 0, y: 0, width: videoSize.width, height: videoSize.height)
+        videoLayer.frame = CGRect(x: 0, y: 0, width: videoSize.width, height: videoSize.height)
+        parentLayer.addSublayer(videoLayer)
+        parentLayer.addSublayer(animationLayer)
+        
+        //create the composition and add the instructions to insert the layer:
+        
+        let videoComp: AVMutableVideoComposition = AVMutableVideoComposition()
+        videoComp.renderSize = videoSize
+        videoComp.frameDuration = CMTime(value: 1, timescale: 30)
+        videoComp.animationTool = AVVideoCompositionCoreAnimationTool(postProcessingAsVideoLayer: videoLayer, in: parentLayer)
+        
+        /// instruction
+        let instruction: AVMutableVideoCompositionInstruction = AVMutableVideoCompositionInstruction()
+        
+        instruction.timeRange = CMTimeRangeMake(start: CMTime.zero, duration: mixComposition.duration)
+        guard let mixVideoTrack: AVAssetTrack = mixComposition.tracks(withMediaType: .video).first else {
+            assertionFailure("Unable to video track")
+            return
+        }
+        let layerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: mixVideoTrack)
+        instruction.layerInstructions = [layerInstruction]
+        videoComp.instructions = [instruction]
+        
+        // export video
+        guard let assetExport = AVAssetExportSession(asset: mixComposition, presetName: AVAssetExportPresetHighestQuality) else {
+            assertionFailure("Unable to create asset export")
+            return
+        }
+        assetExport.videoComposition = videoComp
+        
+        let videoName = "AnimatedVideo.mp4"
+        
+        let animatedVideoURL = videoURL.deletingLastPathComponent().appendingPathComponent(videoName)
+        assetExport.outputFileType = AVFileType.mp4
+        assetExport.outputURL = animatedVideoURL
+        assetExport.shouldOptimizeForNetworkUse = true
+        
+        assetExport.exportAsynchronously { [unowned self] in
+            
+            switch assetExport.status {
+                
+            case .waiting, .unknown, .exporting:
+                debugPrint("Export status: \(assetExport.status.rawValue)")
+                
+            case .completed:
+                success(animatedVideoURL)
+                
+            case .failed:
+                failure(self.failedToExportAssetError)
+                
+            case .cancelled:
+                failure(self.canceledExportAssetError)
+                
+            @unknown default:
+                fatalError()
+            }
+        }
     }
     
 }
