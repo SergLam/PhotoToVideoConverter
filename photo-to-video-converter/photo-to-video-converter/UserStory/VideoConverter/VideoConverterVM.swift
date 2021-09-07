0fb461a6f5d38bd8044046a62d20de0d1df99458
SergLam <lamcevs0@gmail.com>
2021-09-08 02:45:08 +0300

    project update

--- photo-to-video-converter/photo-to-video-converter/UserStory/VideoConverter/VideoConverterVM.swift
+++ photo-to-video-converter/photo-to-video-converter/UserStory/VideoConverter/VideoConverterVM.swift
@@ -10,7 +10,7 @@ import UIKit
 import AVFoundation
 import Photos
 
-protocol VideoConverterVMDelegate: class {
+protocol VideoConverterVMDelegate: AnyObject {
     
     func didReceivedError(error: String)
     func didReceiveSuccess(message: String)
